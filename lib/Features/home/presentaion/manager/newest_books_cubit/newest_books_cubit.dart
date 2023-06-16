da47e46401ead4324cad1b89d50d1e7e385de1ce
Alhassan-Ali7 <132752068+Alhassan-Ali7@users.noreply.github.com>
2023-06-16 19:55:00 +0300

    get_it

--- lib/Features/home/presentaion/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/Features/home/presentaion/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,19 +1,19 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit(this.newestBooksUseCase) : super(NewestBooksInitial());
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
 
-  final FetchNewestBooksUseCase newestBooksUseCase;
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
 
   Future<void> fetchNewestBooks() async{
     emit(NewestBooksLoading());
 
-    var result = await newestBooksUseCase.call();
+    var result = await fetchNewestBooksUseCase.call();
 
     result.fold((failure) {
       emit(NewestBooksFailure(failure.message));
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
+import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookly_app/Features/home/presentaion/manager/featured_books_cuibt/featured_books_cubit.dart';
+import 'package:bookly_app/Features/home/presentaion/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/funnctions/setup_service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter();
 
   Hive.registerAdapter(BookEntityAdapter());
+
+  setupServiceLocator();
 
   await Hive.openBox<BookEntity>(kFeaturedBox);
 
@@ -17,23 +26,44 @@ void main() async{
   runApp(const BooklyApp());
 }
 
+
+
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return FeaturedBooksCubit(
+              FetchFeaturedBooksUseCase(
+                getIt.get<HomeRepoImpl>(),
+              ),
+            );
+          },
         ),
+        BlocProvider(
+          create: (context) {
+            return NewestBooksCubit(
+              FetchNewestBooksUseCase(
+                getIt.get<HomeRepoImpl>(),
+              ),
+            );
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-
-      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-
